3534ddde819e5bcceaf4ad1f5aee9f9f51925811
Joel Azemar <joel.azemar@gmail.com>
2023-10-31 11:43:48 +0100

    Change Spec

--- spec/gem_b_spec.rb
+++ spec/gem_b_spec.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
-
-RSpec.describe GemB do
-  it "has a version number" do
-    expect(GemB::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+RSpec.describe GemB
